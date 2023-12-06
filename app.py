37f417be6192f481e1c3c26896080cda2923784c
Charles Chitty <charlescchitty@gmail.com>
2023-12-06 15:16:43 -0800

    Added flask skeleton

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db
+from models import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +14,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+app.config['SECRET_KEY'] = "oh-so-secret"
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+@app.get("/")
+def index():
+    """
+    Creates homepage HTML.
+    """
+
+    return render_template("homepage.html")
+    """It should be able to import the User model, and create the
+    tables using SQLAlchemy. Make sure you have the FlaskDebugToolbar
+    installed — it’s especially helpful when using SQLAlchemy."""
+
+@app.post("/")
+def add_user():
+    """
+    Add user and redirect to list of users
+    """
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url'] #is there a command for like.get(x,None)
+    image_url = image_url if image_url else None
+
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(User)
+    db.session.commit()
+
+    return redirect(f"/{user.id}")
+
--- models.py
+++ models.py
@@ -1,1 +1,41 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User.
+
+    Fields:
+    -dept_code: xxx
+    -dept_name: yyy
+
+    Relationship:
+    -employees : returns lsit of [EmployeeInstnace, ...]
+    """
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrementing=True
+    )
+
+    first_name =  db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(50),
+    )
+
+    def __repr__(self):
+        return f"<User {self.first_name} {self.last_name} id={self.id}>"
